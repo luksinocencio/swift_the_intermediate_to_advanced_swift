24c05f8955137be824baa6bd3a7d9926afc245e6
luksinocencio <luksinocencio@gmail.com>
2022-01-12 19:02:01 -0300

    *

---
+++ Section02 - Chapter 01 - The Swift Fundamentals/Lesson11 - Intro to Operators.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+//: Problem: Write less, produce more
+//: Definition -> An operator is a symbol for a function
+
+/// Unary operators
+!true //false
+!false // true
+
+//: Binary operators
+1 + 2
+4 == 4
+1 / 4
+5 % 2
+
+//: ### Ternary Operators
+//: Typical Else-if
+let iCanDrink = false
+
+if iCanDrink {
+    print("You may enter")
+} else {
+    print("No no")
+}
+
+//: Tertiary Operator
+iCanDrink ? print("You may enter") : print("No no")
+
+var evenNumbers: [Int] = []
+var oddNumbers: [Int] = []
+
+for number in 1...50 {
+    (number % 2 == 0) ? evenNumbers.append(number) : oddNumbers.append(number)
+}
+
+//: Unwrapping Optionals
+var age: Int? = nil
+var defaultAge: Int = 20
+
+var finalUserAge = Int() // 0
+
+if age != nil {
+    finalUserAge = age!
+} else {
+    finalUserAge = defaultAge
+}
+
+//: Nil-Coalescing Operator
+
+finalUserAge = age ?? defaultAge
